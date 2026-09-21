1b6f54aade3d27ae7dafa10bc727c02743a41005
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-09-22 02:18:00 +0330

    add configs and loggin setup for project logger

--- v6/app/main.py
+++ v6/app/main.py
@@ -1,16 +1,27 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from
+
+from v5.app.db.chroma import settings
+from .core import get_settings, get_logger, setup_logging
+
+logger = get_logger(__name__)
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     try:
-        print("start of the project")
+        setup_logging()
+        logger.info("Application starting up")
+
         yield
-        print("shutdown woth no problem")
+
+        logger.info("Application shutting down")
     except Exception as e:
-        print(f"we can not countinue for there because of the error:\n {e}")
+        logger.error(f"we can not countinue for there because of the error:\n {e}")
 
 app = FastAPI(lifespan=lifespan)
 
 if __name__ == "__main__":
     import uvicorn
+
+    settings = get_settings()
+    uvicorn.run(app=app, host=settings.app_host, port=settings.app_port)
