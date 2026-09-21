fb683c9a0284b51e9e96af559a09879e510db96d
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-09-22 02:59:12 +0330

    S3 GARAGE OBJECT STORAGE

--- v6/app/core/config.py
+++ v6/app/core/config.py
@@ -20,9 +20,16 @@ class Settings(BaseSettings):
     embedding_model: str = Field(..., validation_alias="EMBEDDING_MODEL")
 
     ### Qdrant ###
-    QDRANT_HOST: str = Field(..., validation_alias="QDRANT_HOST")
-    QDRANT_PORT: int = Field(..., validation_alias="QDRANT_PORT")
-    QDRANT_COLLECTION: str = Field(..., validation_alias="QDRANT_COLLECTION")
+    qdrant_host: str = Field(..., validation_alias="QDRANT_HOST")
+    qdrant_port: int = Field(..., validation_alias="QDRANT_PORT")
+    qdrant_collection: str = Field(..., validation_alias="QDRANT_COLLECTION")
+
+    ### S3 GARAGE OBJECT STORAGE ###
+    garage_endpoint: str = Field(..., validation_alias="GARAGE_ENDPOINT")
+    garage_region: str = Field(..., validation_alias="GARAGE_REGION")
+    garage_access_key: str = Field(..., validation_alias="GARAGE_ACCESS_KEY")
+    garage_scret_key: str = Field(..., validation_alias="GARAGE_SECRET_KEY")
+    garage_bucket: str = Field(..., validation_alias="GARAGE_BUCKET")
 
 @lru_cache
 def get_settings():
